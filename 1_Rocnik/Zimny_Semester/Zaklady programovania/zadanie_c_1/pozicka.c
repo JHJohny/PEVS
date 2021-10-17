5e08013ea692bdef43bf722b8f10efd422cc8234
JHJohny <ondis.jan1@gmail.com>
2021-10-17 08:36:40 +0200

    Initial commit with already pre-worked templates / notes. Basically moving all my stuff to GitHub.

---
+++ 1_Rocnik/Zimny_Semester/Zaklady programovania/zadanie_c_1/pozicka.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main( ) {
+
+   char str[100];
+   int i;
+
+   printf( "Enter a value :");
+   scanf("%s", str);
+
+   printf( "\nYou entered: %s ", str);
+
+   return 0;
+}
