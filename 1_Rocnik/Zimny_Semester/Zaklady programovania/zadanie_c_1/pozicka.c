c0c917c90854dc4c8d49e0e581e062a1fa27946c
JHJohny <ondis.jan1@gmail.com>
2021-10-23 08:27:49 +0200

    Basic user input reading - DONE

--- 1_Rocnik/Zimny_Semester/Zaklady programovania/zadanie_c_1/pozicka.c
+++ 1_Rocnik/Zimny_Semester/Zaklady programovania/zadanie_c_1/pozicka.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char *substring()
+{
+    char *ch1;
+    ch1=malloc(sizeof(char));
+
+    printf( "Enter a value :");
+    scanf("%s", ch1);
+
+    return ch1;
+}
 
 int main( ) {
 
    char str[100];
-   int i;
+   char *str_two;
 
-   printf( "Enter a value :");
-   scanf("%s", str);
+   //printf( "Enter a value :");
+   //scanf("%s", str);
 
-   printf( "\nYou entered: %s ", str);
+    str_two = substring();
+
+   printf( "\nYou entered: %s ", str_two);
 
    return 0;
 }
