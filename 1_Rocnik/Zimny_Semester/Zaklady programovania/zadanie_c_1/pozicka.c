103a437f157676a3bc5f756153170cc54f77cff9
JHJohny <ondis.jan1@gmail.com>
2021-10-23 16:22:27 +0200

    Digit only is function is DONE

--- 1_Rocnik/Zimny_Semester/Zaklady programovania/zadanie_c_1/pozicka.c
+++ 1_Rocnik/Zimny_Semester/Zaklady programovania/zadanie_c_1/pozicka.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 char *getUsersInput()
 {
@@ -12,11 +14,30 @@ char *getUsersInput()
     return ch1;
 }
 
+int isDigitOnly(const char *s)
+{
+    for (int i = 0; i < strlen(s); i++) {
+        if(isdigit(s[i]) == 0){
+            if(!(s[i] == '.' || s[i] == ',')){
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
 int main( ) {
     char *userInput;
     userInput = getUsersInput();
 
-    printf( "\nYou entered: %s \n", userInput);
+    printf( "\nYou entered: %s \n\n", userInput);
+
+    if(isDigitOnly(userInput)){
+        printf( "And it's digit only\n");
+    }else{
+        printf( "And it's NOT digit only\n");
+    }
 
     return 0;
 }
