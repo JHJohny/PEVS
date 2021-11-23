b5006be6a1de482b30b9992189664d4b846f32a9
JHJohny <ondis.jan1@gmail.com>
2021-11-23 20:36:16 +0100

    It's DONE

--- 1_Rocnik/Zimny_Semester/Zaklady programovania/zadanie_c_3/kalendar.c
+++ 1_Rocnik/Zimny_Semester/Zaklady programovania/zadanie_c_3/kalendar.c
@@ -37,14 +37,56 @@ int isDigitOnly(const char *s)
     return 1;
 }
 
+void print_month(char *month, int* week_day_index, int months_days) {
+    /*
+    This function print the whole month.
 
+    This function print the whole month, based on where the week should start (day index) - Mon, Thue...
+    Function use a week index pointer - so the next iteration of this function (next month) could start where the
+    previous one finished.
+    */
+    printf("\n\n");
+    printf("          %s        \n", month);
+    printf(" P   U   S   Š   P   S   N \n");
+    printf("---------------------------\n");
+
+    int j=1; //Just a holder of the current routing day in a WEEK
+    int k=1; //Just a hodler of the current routing day in a MONTH
+    for(int i=1; i<=months_days; i++) {
+        if(j==*week_day_index) {
+
+            //Handle single/double digits days
+            if(k < 10) {
+                printf(" 0%d ", k);
+            }else {
+                printf(" %d ", k);
+            }
+
+            *week_day_index+=1;
+            j+=1;
+            k+=1;
+
+            //Week print finished - move to another one
+            if(*week_day_index==8) {
+                *week_day_index=1;
+                j=1;
+                printf("\n"); //Week ended - make a new line for a another one
+            }
+        }else {
+            //We are handling the empty days
+            j+=1; //We are moving to a next day in a week
+            i--; //We actually didn't print a day number - so we need to decrement and just print a space holder
+            printf("    ");
+        }
+    }
+}
 
 int main() {
     int selected_year;
     int selected_starting_day;
 
-    int months_days_number[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};;
-    char *months_names[13] = {"empty", "Januar", "Februar", "Marec", "April", "Maj", "Jun", "Jul", "August", "September", "Oktober", "November", "December"};
+    int months_days_number[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    char *months_names[12] = {"Januar", "Februar", "Marec", "April", "Maj", "Jun", "Jul", "August", "September", "Oktober", "November", "December"};
 
     //Handle proper year entering
     while(1) {
@@ -76,8 +118,15 @@ int main() {
 
     //Gap year
     if(selected_year % 4 == 0) {
-        months_days_number[2] = 29;
+        months_days_number[1] = 29;
     }
+
+    for(int i=0; i < 12; i++) {
+        //Printing months one by one
+        print_month(months_names[i], &selected_starting_day, months_days_number[i]);
+    }
+
+    printf("\n"); //Just leave a empty line once the program finish.
 
     return 0;
 }
