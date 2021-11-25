8283a9d42cbc4ef33695f09d74035b78aa8b545e
JHJohny <ondis.jan1@gmail.com>
2021-11-25 21:56:30 +0100

    Basic reading is DONE

--- 1_Rocnik/Zimny_Semester/Zaklady programovania/zadanie_c_4/cipher.c
+++ 1_Rocnik/Zimny_Semester/Zaklady programovania/zadanie_c_4/cipher.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
+void readNumbersFromfile(char *file_name_with_path, int max_number) {
+    FILE* f = fopen(file_name_with_path, "r");
+    int number = 0;
+
+    while( fscanf(f, "%d,", &number) > 0 )
+    {
+        if(number > max_number) {
+            printf("Number greater than %d was found! This number will be excluded from the program! - %d", max_number, number);
+        }else {
+            printf("This is the number %d\n", number);
+        }
+    }
+
+    fclose(f);
+}
+
 int main() {
-    char *crypto_file_name="text.txt";
+    char *crypted_file_name="text.txt";
     char *key_file_name="key.txt";
     char *descrypted_file_name="translation.txt";
+
+    readNumbersFromfile(crypted_file_name);
 
     return 0;
 }
