0db9ebe0d0dad2274f0c063190b550e88c348f50
JHJohny <ondis.jan1@gmail.com>
2021-12-05 13:19:37 +0100

    Decrypting is DONE - TODO save it to the file + do the testing

--- 1_Rocnik/Zimny_Semester/Zaklady programovania/zadanie_c_4/cipher.c
+++ 1_Rocnik/Zimny_Semester/Zaklady programovania/zadanie_c_4/cipher.c
@@ -15,6 +15,46 @@ void readNumbersFromfile(char *file_name_with_path) {
     fclose(f);
 }
 
+char *ReadFile(char *filename)
+{
+   char *buffer = NULL;
+   int string_size, read_size;
+   FILE *handler = fopen(filename, "r");
+
+   if (handler)
+   {
+       // Seek the last byte of the file
+       fseek(handler, 0, SEEK_END);
+       // Offset from the first to the last byte, or in other words, filesize
+       string_size = ftell(handler);
+       // go back to the start of the file
+       rewind(handler);
+
+       // Allocate a string that can hold it all
+       buffer = (char*) malloc(sizeof(char) * (string_size + 1) );
+
+       // Read it all in one operation
+       read_size = fread(buffer, sizeof(char), string_size, handler);
+
+       // fread doesn't set it so put a \0 in the last position
+       // and buffer is now officially a string
+       buffer[string_size] = '\0';
+
+       if (string_size != read_size)
+       {
+           // Something went wrong, throw away the memory and set
+           // the buffer to NULL
+           free(buffer);
+           buffer = NULL;
+       }
+
+       // Always remember to close the file.
+       fclose(handler);
+    }
+
+    return buffer;
+}
+
 char findLetterAfterInt(char *my_string, int num) {
     char *num_as_string = malloc(sizeof(char));
     sprintf(num_as_string, "%d", num);
@@ -53,10 +93,35 @@ char findLetterAfterInt(char *my_string, int num) {
     return (char) 0; //If nothing was found - return nothing
 }
 
+void decipherTheText(char *keys_file_name, char *crypted_file_name, char *decrypted_file_name) {
+    //TODO append to the array
+
+    //Firstly we will read number by number from the crypted file name
+    FILE* f_crypted_file = fopen(crypted_file_name, "r"); //Read the text - where the message is crypted
+    int number = 0;
+
+    char *key_file_content = ReadFile(keys_file_name);
+
+    while( fscanf(f_crypted_file, "%d,", &number) > 0 )
+    {
+        char decrypted_letter = findLetterAfterInt(key_file_content, number);
+
+        printf("%c", decrypted_letter);
+    }
+
+    fclose(f_crypted_file);
+
+    printf("\n\n"); //Just a aesthetic
+}
+
 int main() {
     char *crypted_file_name="text.txt";
     char *key_file_name="key.txt";
     char *descrypted_file_name="translation.txt";
+
+    decipherTheText(key_file_name, crypted_file_name, descrypted_file_name);
+
+    return 0;
 
     char tmp_text[] = "This is my test temporary string 12 g svdvf";
 
