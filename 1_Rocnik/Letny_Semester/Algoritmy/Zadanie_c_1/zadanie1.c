1701ac793470729026c6d57a6a458f3e2fff8a74
JHJohny <ondis.jan1@gmail.com>
2022-03-12 10:15:40 +0100

    Notes are DONE.

---
+++ 1_Rocnik/Letny_Semester/Algoritmy/Zadanie_c_1/zadanie1.c
@@ -0,0 +1,8 @@
+
+//Zadanie 1 is sorting algorithm related
+//in assignment, there is explicitly said to use structures, but I don't think it's a necessary.
+//If I will find a way how to move entire lines of text within the file - I won't have to create nth structures
+//I will save nth times of memory
+//Todo - find an appropiate sorting algorithm for this assignment > probably Radix sort
+//Todo - find if it's a possible to move lines of code within the file - so I won't have to use structure
+
