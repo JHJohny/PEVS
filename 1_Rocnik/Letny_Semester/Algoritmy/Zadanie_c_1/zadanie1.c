8f29843947578cc65f6eb4f70b569801e6feab46
JHJohny <ondis.jan1@gmail.com>
2022-03-20 09:44:19 +0100

    Commit

--- 1_Rocnik/Letny_Semester/Algoritmy/Zadanie_c_1/zadanie1.c
+++ 1_Rocnik/Letny_Semester/Algoritmy/Zadanie_c_1/zadanie1.c
@@ -4,7 +4,6 @@
 
 //Zadanie 1 is sorting algorithm related
 //in assignment, there is explicitly said to use structures, but I don't think it's a necessary.
-//Logic - instead of allocating memory for the entire file I could read line by line and insert the elements in order with the binary search
 
 typedef struct zasielkaStruct
 {
@@ -44,11 +43,78 @@ void getZasielkyFromFile(zasielka *zasielky, char* fileName)
     fclose(fileHandler);
 }
 
+
+void countingSort(int a[], int n, int place) // function to implement counting sort
+{
+    printf("This is array ");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    int output[n + 1];
+    int occurences[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //This array represent occurence of each digit from the nth place
+
+    //Looping trough the list and recording the count of digit from the nth place
+    for (int i = 0; i < n; i++)
+    {
+        int moduleForElement = ((a[i] / place) % 10); //This modulo represents digit from 0-10 from the nth place we are sorting
+
+        occurences[moduleForElement]++;
+    }
+
+    //Summing out the values with the predecessor values - cumulative sum
+    for (int i = 1; i < 10; i++)
+    {
+        occurences[i] += occurences[i - 1];
+    }
+
+    for(int i = 0; i < (n - 1); i++)
+    {
+        int inputValue = (a[i] / place);
+        int indexFromOccurences = occurences[inputValue];
+
+        output[indexFromOccurences] = inputValue;
+        occurences[inputValue] = (occurences[inputValue]--);
+    }
+
+    /*
+    //Sort numbers in order, based on the nth place
+    for (int i = n - 1; i >= 0; i--)
+    {
+        output[occurences[(a[i] / place) % 10] - 1] = a[i];
+        occurences[(a[i] / place) % 10]--;
+    }
+    */
+
+    for (int i = 0; i < n; i++)
+        a[i] = output[i];
+}
+
+// function to implement radix sort
+void radixsortCustom(int a[], int n) {
+
+    int place = 1;
+    for(int i = 1; i < 5; i++) //We are sorting up to 9999 priority > 1st, 10nth, 100nth, 1000nth places
+    {
+        countingSort(a, n, place);
+        place *= 10;
+    }
+}
+
 int main()
 {
-    zasielka *zasielky = (zasielka*) malloc(sizeof(zasielka) * 100000);
+    /*
+    zasielka *zasielky = (zasielka*) malloc(sizeof(zasielka) * 100000); //Creating list of zasielky
 
     getZasielkyFromFile(zasielky, "./DataExample.txt");
+    */
+
+    int fictionalZasielky[] = {1, 90, 11, 20, 43, 29, 88, 77, 54, 48, 85, 92, 9999, 9922, 9123};
+    int lengthOfZasielky = sizeof fictionalZasielky / sizeof fictionalZasielky[0];
+
+    radixsortCustom(fictionalZasielky, lengthOfZasielky);
 
     return 0;
 }
