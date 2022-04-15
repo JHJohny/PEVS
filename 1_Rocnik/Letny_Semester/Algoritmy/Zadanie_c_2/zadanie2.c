94a77369db89e7f3b36be0cf8aed3172d1880ce5
Johnny Ondis <46457630+JHJohny@users.noreply.github.com>
2022-04-15 20:42:27 +0200

    Update zadanie2.c
    Binary search added

--- 1_Rocnik/Letny_Semester/Algoritmy/Zadanie_c_2/zadanie2.c
+++ 1_Rocnik/Letny_Semester/Algoritmy/Zadanie_c_2/zadanie2.c
@@ -3,7 +3,7 @@
 
 int customBruteForceSearch(int arr[], int length, int num) // I could dynamically get length of array - but that wouldn't be sufficient
 {
-    // Doesn't return value of index of the searched element, but number of comparisions 
+    // Doesn't return value of index of the searched element, but number of comparisions
 
     int nthIterationIndex = 0; // Temporary index for storing how many times did we do comparison
     for(int i = 0; i < length; i++)
@@ -19,11 +19,37 @@ int customBruteForceSearch(int arr[], int length, int num) // I could dynamicall
     return nthIterationIndex; // Technically we could just return i, but
 }
 
-int customBinarySearch()
+int customBinarySearch(int arr[], int length, int num)
 {
     // Doesn't return value of index of the searched element, but iteration
 
-    return 0;
+    int l = 0;
+    int r = length -1;
+    int comparisions = 0;
+
+    while(l <= r)
+    {
+        comparisions++;
+        int mid = l + (r - l) / 2;
+
+        comparisions++;
+        if(arr[mid] == num)
+        {
+            return comparisions;
+        }
+
+        comparisions++;
+        if(arr[mid] < num)
+        {
+            l = mid + 1;
+        }
+
+        comparisions++;
+        if(arr[mid] > num)
+        {
+            r = mid - 1;
+        }
+    }
 }
 
 int main() {
@@ -41,6 +67,8 @@ int main() {
     int randomNumber = rand() % (lengthOfArray + 1 - 0) + 0;
     printf("Random number is %d\n", randomNumber);
 
-    
+    int resultFromBinarySearch = customBinarySearch(arrayWithNums, lengthOfArray, randomNumber);
+    printf("Result is %d\n", resultFromBinarySearch);
+
     return 0;
 }
