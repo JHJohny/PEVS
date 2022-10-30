55dbb30dd5977d53e24484090ee27ba1b3c38c73
JHJohny <ondis.jan1@gmail.com>
2022-10-30 21:18:36 +0100

    Some progress is made - prioritized que is done

--- 2_rocnik/Zimny_Semester/OOP/Zadania/Zadanie_2/Main.java
+++ 2_rocnik/Zimny_Semester/OOP/Zadania/Zadanie_2/Main.java
@@ -1,4 +1,7 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.Date;
+import java.util.PriorityQueue;
 
 interface Task {
     public void TakeOverTheTask(); //The task is now being handled by user
@@ -6,16 +9,14 @@ interface Task {
 }
 
 class FIKTIVTask implements Task {
-    public enum taskTypes { administration, complaint };
-
-    private taskTypes taskType;
     private String Description;
     private Date creationDate;
-    private int daysPostponed;
     private boolean prioritized;
 
-    public FIKTIVTask (String _string) {
-        // TODO
+    public FIKTIVTask (String _description, Date _creationDate, boolean _prioritized) {
+        setDescription(_description);
+        setCreationDate(_creationDate);
+        setPrioritized(_prioritized);
     }
 
     public void TakeOverTheTask () {
@@ -25,10 +26,67 @@ class FIKTIVTask implements Task {
     public void SolveTheTask () {
         // TODO
     }
+
+    // ### Getters and setters ### //
+    public boolean isPrioritized() {
+        return prioritized;
+    }
+
+    public void setPrioritized(boolean prioritized) {
+        this.prioritized = prioritized;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
 }
 
+class AdministrationTask extends FIKTIVTask{
+
+    private int daysPostponed;
+
+    public AdministrationTask(String _string) {
+        super(_string);
+    }
+
+    public void PostponeTask () {
+        daysPostponed += 1;
+    }
+
+    @Override
+    public Date getCreationDate() {
+        System.out.println("Subclass");
+        return super.getCreationDate(); //TODO do summary
+    }
+}
+
+class ComplaintTask extends FIKTIVTask{
+
+    public ComplaintTask(String _string) {
+        super(_string);
+    }
+
+    public void CancelTask () {
+
+    }
+}
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+
     }
 }
